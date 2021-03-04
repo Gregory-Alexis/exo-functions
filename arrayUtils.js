3bae2ebc7dd69f70a45d01f1aa7d7436093ec7b0
Westindiess <gregoryalexiss@gmail.com>
2021-03-04 14:46:15 +0100

    exo fonctions

--- arrayUtils.js
+++ arrayUtils.js
@@ -1,5 +1,19 @@
-const biggest = (a, b, c, d) => {
+/* const biggest = (a, b, c, d) => {
   return Math.max(a, b, c, d)
 }
 
-console.log(biggest(99, 100, 101, 1))
+console.log(biggest(99, 100, 101, 1)) */
+
+/* let sortAscend = [99, 100, 101, 1];
+
+sortAscend.sort((a, b) => {
+  return a - b
+});
+console.log(sortAscend) */
+
+
+
+const makeUniques = (arr) => {
+  return [...new Set(arr)]
+}
+console.log(makeUniques([1, 2, 1, 3, 2, 4, 5, 7, 5, 1]))
