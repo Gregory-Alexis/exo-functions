64f0394ee664d9da3c5485ae1690615ae4207f10
Westindiess <gregoryalexiss@gmail.com>
2021-03-05 12:53:38 +0100

    exo fonctions

---
+++ filter.js
@@ -0,0 +1,24 @@
+isEven = (number) => {
+  return number % 2 === 0 ? true : false
+}
+
+
+
+
+const filter = (func, array) => {
+  const filteredTab = []
+  for (const elem of array) {
+    if (func(elem)) {
+      filteredTab.push(elem)
+    }
+  }
+  return filteredTab
+}
+
+
+numbers = [12, 1, 43, 3, 8, 9, 22, 6, 19, 88, 76]
+
+
+//let newTab = numbers.filter(isEven)
+//console.log(newTab)
+console.log(filter(isEven, numbers))
