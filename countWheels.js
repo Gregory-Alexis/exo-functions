8c7b18b2ddec2d078c32035ff7060a6c7fe2d1cb
Westindiess <gregoryalexiss@gmail.com>
2021-03-05 12:14:48 +0100

    exo fonctions

--- countWheels.js
+++ countWheels.js
@@ -1,5 +1,11 @@
+const NB_WHEELS_CYCLE = 1
+const NB_WHEELS_MOTO = 2
+const NB_WHEELS_CAR = 4
+const NB_WHEELS_LIMO = 6
+
+
 const countWheels = (nb_monocycle, nb_moto, nb_voiture, nb_limousine) => {
-  return nb_monocycle * 1 + nb_moto * 2 + nb_voiture * 4 + nb_limousine * 6
+  return nb_monocycle * NB_WHEELS_CYCLE + nb_moto * NB_WHEELS_MOTO + nb_voiture * NB_WHEELS_CAR + nb_limousine * NB_WHEELS_LIMO
 
 }
 
